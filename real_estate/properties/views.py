7052510e384d972c4e4d5a9efcb96d269601e05a
Babcowpy <babkow.py@gmail.com>
2024-01-24 23:15:59 +0300

    start3

--- real_estate/properties/views.py
+++ real_estate/properties/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Property
+
+def property_list(request):
+       properties = Property.objects.all()
+       return render(request, 'property_list.html', {'properties': properties})
+
+def property_detail(request, property_id):
+       property = get_object_or_404(Property, pk=property_id)
+       return render(request, 'property_detail.html', {'property': property})
--- real_estate/real_estate/urls.py
+++ real_estate/real_estate/urls.py
@@ -20,3 +20,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+       path('', views.property_list, name='property_list'),
+       path('property/<int:property_id>/', views.property_detail, name='property_detail'),
+   ]
