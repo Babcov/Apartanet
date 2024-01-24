d44969485b2b487a5275d3356cc869d3391fb5f4
Babcowpy <babkow.py@gmail.com>
2024-01-24 22:29:08 +0300

    start2

--- real_estate/properties/models.py
+++ real_estate/properties/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Property(models.Model):
+       title = models.CharField(max_length=255)
+       description = models.TextField()
+       price = models.DecimalField(max_digits=10, decimal_places=2)
+       bedrooms = models.IntegerField()
+       bathrooms = models.DecimalField(max_digits=4, decimal_places=2)
+       # Добавьте остальные поля модели
--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'properties'
 ]
 
 MIDDLEWARE = [
@@ -74,11 +76,12 @@ WSGI_APPLICATION = 'real_estate.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+       'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       }
+   }
+
 
 
 # Password validation
